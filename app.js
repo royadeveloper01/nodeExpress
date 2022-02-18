22c7cad8189e0520f35db25ec5d1f198672ff9f9
Chimaobi Prince <royadeveloper01@gmail.com>
2022-02-18 21:04:40 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const rootDir = require('./util/path');
+
+const app = express();
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(rootDir, 'views', 'error-page.html'))
+});
+
+app.listen(3000); 
