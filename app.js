da08abd49782febb59089976a5aac438b0c35cb4
Chimaobi Prince <royadeveloper01@gmail.com>
2022-02-19 03:43:53 +0100

    added templating engines

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const rootDir = require('./util/path');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
--- routes/admin.js
+++ routes/admin.js
@@ -16,7 +16,6 @@ router.get('/add-product', (req, res, next) => {
 // Admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
     products.push({ title: req.body.title });
-    // console.log(products);
     res.redirect('/');
 });
 
