50c795f73d3cd71c3a7468ba778630b503dc89f8
Chimaobi Prince <royadeveloper01@gmail.com>
2022-02-28 03:58:12 +0100

    update

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
     Product.fecthAll(products => {
@@ -16,10 +17,10 @@ exports.getProduct = (req, res, next) => {
         res.render('shop/product-detail', {
             product: product,
             pageTitle: product.title,
-            path: '/'
+            path: '/products'
         })
     })
-}
+};
 
 exports.getIndex = (req, res, next) => {
     Product.fecthAll(products => {
@@ -36,18 +37,26 @@ exports.getCart = (req, res, next) => {
         path: '/cart',
         pageTitle: 'Your Cart'
     });
-}
+};
+
+exports.postCart = (req, res, next) => {
+    const prodId = req.body.productId;
+    Product.findById(prodId, product => {
+        Cart.addProduct(prodId, product.price)
+    });
+    res.redirect('/cart');
+};
 
 exports.getOrders = (req, res, next) => {
     res.render('shop/orders', {
         path: '/orders',
         pageTitle: 'Your Orders'
     });
-}
+};
 
 exports.getCheckout = (req, res, next) => {
     res.render('shop/checkout', {
         path: '/checkout',
         pageTitle: 'Checkout'
     });
-}
+};
