a0a46c63ead33888d70d1e20543508c0f472b8d3
Chimaobi Prince <royadeveloper01@gmail.com>
2022-04-13 11:37:30 +0100

    add cart functionality on MongoDB

--- models/product.js
+++ models/product.js
@@ -2,12 +2,13 @@ const getDb = require('../util/database').getDb;
 const mongodb = require('mongodb');
 
 class Product {
-    constructor(title, imageUrl, description, price, id) {
+    constructor(title, imageUrl, description, price, id, userId) {
         this.title = title;
         this.imageUrl = imageUrl;
         this.description = description;
         this.price = price;
         this._id = id ? new mongodb.ObjectId(id) : null;
+        this.userId = userId;
     }
 
     save() {
@@ -24,7 +25,7 @@ class Product {
         }
         return dbOp
         .then(result => {
-            console.log(result);
+            // console.log(result);
         })
         .catch(err => {
             console.log(err);
